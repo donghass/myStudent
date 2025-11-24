56a4d8455ae8ec9c3f188d81baa148bb4e645dd9
donghass <ehdgk1221@gmail.com>
2025-11-25 00:29:56 +0900

    학생테이블 수정

--- src/main/java/kr/myStudent/student/repository/JpaStudentRepository.java
+++ src/main/java/kr/myStudent/student/repository/JpaStudentRepository.java
@@ -1,10 +1,9 @@
 package kr.myStudent.student.repository;
 
 import kr.myStudent.student.domain.StudentEntity;
-import kr.myStudent.student.domain.StudentId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JpaStudentRepository extends JpaRepository<StudentEntity, StudentId> {
+public interface JpaStudentRepository extends JpaRepository<StudentEntity, Long> {
 }
