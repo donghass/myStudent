29c35395b342ef07b9d3bb2f37809def8c07a13e
CHAN <cksgh1565@gmail.com>
2025-11-27 11:50:49 +0900

    feat: Redis SSL 옵션 추가

--- src/main/java/kr/myStudent/config/RedisConfig.java
+++ src/main/java/kr/myStudent/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -19,7 +20,12 @@ public class RedisConfig {
             @Value("${spring.data.redis.token}") String token) {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
         config.setPassword(token);
-        return new LettuceConnectionFactory(config);
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .useSsl()
+                .build();
+
+        return new LettuceConnectionFactory(config, clientConfig);
     }
 
     @Bean
